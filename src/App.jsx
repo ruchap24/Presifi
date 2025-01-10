8da407e2387bfeeceb91e4f1c864d6029cfcb3b6
ruchap24 <patilrucha991@gmail.com>
2025-01-10 13:00:48 +0530

    components linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import About from "./components/about";
+import Cards from "./components/Cards";
+import Eyes from "./components/Eyes";
+import Featured from "./components/Feature";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import Marque from "./components/Marque";
+import Navbar from "./components/Navbar";
+import ReadyTo from "./components/ReadyTo";
+
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white'>
-      <Navbar/>
+    <div className="w-full min-h-screen text-white bg-zinc-900">
+      <Navbar />
+      <LandingPage />
+      <Marque />
+      <About />
+      <Eyes />
+      <Featured />
+      <Cards />
+      {/* <ReadyTo /> */}
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // rfce then enter
