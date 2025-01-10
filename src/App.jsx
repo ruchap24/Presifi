168dcb8faa338b8ea9929155aac464c86e78a0de
ruchap24 <patilrucha991@gmail.com>
2025-01-10 12:36:34 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900'></div>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      <Navbar/>
+    </div>
   )
 }
 
 export default App
+
+// rfce then enter
